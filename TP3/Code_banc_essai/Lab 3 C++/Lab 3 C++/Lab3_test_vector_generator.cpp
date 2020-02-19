d81fcb2b1701f3b04d16ef671ce08c8eb05ea168
Alexandre Morinvil <almori@gigl.polymtl.ca>
2020-02-18 19:25:37 -0500

    Reconception du code pour le rendre plus général

--- TP3/Code_banc_essai/Lab 3 C++/Lab 3 C++/Lab3_test_vector_generator.cpp
+++ TP3/Code_banc_essai/Lab 3 C++/Lab 3 C++/Lab3_test_vector_generator.cpp
@@ -4,372 +4,187 @@
 #include <math.h>
 #include <time.h>
 #include <iostream>
-
+#include <cstring>
 #include <vector>
+#include <string>
 
-#define NOMBRE_VALEURS		64
+#include "fonctions_TP3.h"
 
+#define NOMBRE_VALEURS 64
+#define MAXIMUM_32BITS 0xFFFFFFFF
+
+// Définition de type
 typedef uint32_t u32;
-const u32 valMax = 0xffffffff;
+typedef std::vector<std::vector<u32>> ListeVecteurs;
 
-// Fonctions à tester
-u32 ch(u32 x, u32 y, u32 z);
-u32 maj(u32 x, u32 y, u32 z);
-u32 sigma0(u32 x);
-u32 sigma1(u32 x);
-u32 sigma2(u32 x);
-u32 sigma3(u32 x);
-u32 rotr(u32 n, u32 x);
-u32 shr(u32 n, u32 x);
-
-u32 inMaj[3][64];
-u32 outMaj[64];
-u32 inCh[3][64];
-u32 outCh[64];
-u32 inSigma0[64];
-u32 outSigma0[64];
-u32 inSigma1[64];
-u32 outSigma1[64];
-u32 inSigma2[64];
-u32 outSigma2[64];
-u32 inSigma3[64];
-u32 outSigma3[64];
-
-
+// Initialisation
 void initialisationGenerateurNombresAleatoires();
 
-std::vector<std::vector<u32>> genererValeursEntree32bitsAleatoires(unsigned nEdgeCases, unsigned nEntrees, unsigned nValeurs);
+// Génération des entrées
+u32 genererValeurAleatoire();
+std::vector<u32> genererVecteurAleatoire(unsigned nValeurs);
+ListeVecteurs genererListeValeursAleatoires(unsigned nEntrees, unsigned nValeurs);
+ListeVecteurs genererListeCasLimitesDeBase(unsigned nEntrees);
+ListeVecteurs genererListeEntrees32bits(unsigned nEntrees, unsigned nValeurs);
+
+// Génération de sorties
+ListeVecteurs genererListeSorties(std::vector<u32>(*fonction)(std::vector<u32>), ListeVecteurs listeEntrees);
+
+// Écriture du fichier de sortie
+void ecrireFichier(const char* nomFichier, ListeVecteurs entrees, ListeVecteurs sorties);
 
 int main()
 {
 	// Initialization
 	initialisationGenerateurNombresAleatoires();
-	genererValeursEntree32bitsAleatoires(8, 3, NOMBRE_VALEURS);
 
-	//// Initialization
-	//srand(time(NULL));
-	//FILE* file;
+	// Génération des entrées
+	ListeVecteurs liste3Entrees = genererListeEntrees32bits(3, NOMBRE_VALEURS);
+	ListeVecteurs liste1Entree  = genererListeEntrees32bits(1, NOMBRE_VALEURS);
 
-	//// INPUT Generation
-	//// Input of  ch function
-	//// INPUT : Edge cases (CH function)
-	//inCh[0][0] = 0;
-	//inCh[0][1] = valMax;
-	//inCh[0][2] = 0;
-	//inCh[0][3] = valMax;
-	//inCh[0][4] = 0;
-	//inCh[0][5] = valMax;
-	//inCh[0][6] = 0;
-	//inCh[0][7] = valMax;
-	//inCh[1][0] = 0;
-	//inCh[1][1] = 0;
-	//inCh[1][2] = valMax;
-	//inCh[1][3] = valMax;
-	//inCh[1][4] = 0;
-	//inCh[1][5] = 0;
-	//inCh[1][6] = valMax;
-	//inCh[1][7] = valMax;
-	//inCh[2][0] = 0;
-	//inCh[2][1] = 0;
-	//inCh[2][2] = 0;
-	//inCh[2][3] = 0;
-	//inCh[2][4] = valMax;
-	//inCh[2][5] = valMax;
-	//inCh[2][6] = valMax;
-	//inCh[2][7] = valMax;
+	// Génération des sorties
+	ListeVecteurs sortiesCh = genererListeSorties(ch, liste3Entrees);
+	ListeVecteurs sortiesMaj = genererListeSorties(maj, liste3Entrees);
+	ListeVecteurs sortiesSigma0 = genererListeSorties(sigma0, liste1Entree);
+	ListeVecteurs sortiesSigma1 = genererListeSorties(sigma1, liste1Entree);
+	ListeVecteurs sortiesSigma2 = genererListeSorties(sigma2, liste1Entree);
+	ListeVecteurs sortiesSigma3 = genererListeSorties(sigma3, liste1Entree);
 
-	//// INPUT : Random values
-	//for (unsigned int j = 0; j < 3; j++)
-	//{
-	//	for (unsigned int i = 8; i < 64; ++i)
-	//	{
-	//		inCh[j][i] = ((RAND_MAX * rand() + rand()) % (valMax + 1)); //on génère entre 0 et 2^32-1 inclus
-	//	}
-	//}
-	//for (unsigned int i = 0; i < 64; ++i)
-	//{
-	//	outCh[i] = ch(inCh[0][i], inCh[1][i], inCh[2][i]);
-	//}
-
-	//// INPUT : Edge cases (MAJ function)
-	//inMaj[0][0] = 0;
-	//inMaj[0][1] = valMax;
-	//inMaj[0][2] = 0;
-	//inMaj[0][3] = valMax;
-	//inMaj[0][4] = 0;
-	//inMaj[0][5] = valMax;
-	//inMaj[0][6] = 0;
-	//inMaj[0][7] = valMax;
-	//inMaj[1][0] = 0;
-	//inMaj[1][1] = 0;
-	//inMaj[1][2] = valMax;
-	//inMaj[1][3] = valMax;
-	//inMaj[1][4] = 0;
-	//inMaj[1][5] = 0;
-	//inMaj[1][6] = valMax;
-	//inMaj[1][7] = valMax;
-	//inMaj[2][0] = 0;
-	//inMaj[2][1] = 0;
-	//inMaj[2][2] = 0;
-	//inMaj[2][3] = 0;
-	//inMaj[2][4] = valMax;
-	//inMaj[2][5] = valMax;
-	//inMaj[2][6] = valMax;
-	//inMaj[2][7] = valMax;
-
-	//for (unsigned int j = 0; j < 3; j++)
-	//{
-	//	for (unsigned int i = 8; i < 64; ++i)
-	//	{
-	//		inMaj[j][i] = ((RAND_MAX * rand() + rand()) % (valMax + 1)); //on génère entre 1 et 2^32-1 inclus
-	//	}
-	//}
-	//for (unsigned int i = 0; i < 64; ++i)
-	//{
-	//	outMaj[i] = maj(inMaj[0][i], inMaj[1][i], inMaj[2][i]);
-	//}
-
-	//// INPUT : sigma0 function
-	//inSigma0[0] = 0;
-	//inSigma0[1] = valMax;
-	//for (unsigned int i = 2; i < 64; ++i)
-	//{
-	//	inSigma0[i] = ((RAND_MAX * rand() + rand()) % (valMax - 1)) + 1; //on génère entre 1 et 2^32-2 inclus
-	//}
-	//for (unsigned int i = 0; i < 64; ++i)
-	//{
-	//	outSigma0[i] = sigma0(inSigma0[i]);
-	//}
-
-	//// INPUT : sigma1 function
-	//inSigma1[0] = 0;
-	//inSigma1[1] = valMax;
-	//for (unsigned int i = 2; i < 64; ++i)
-	//{
-	//	inSigma1[i] = ((RAND_MAX * rand() + rand()) % (valMax - 1)) + 1; //on génère entre 1 et 2^32-2 inclus
-	//}
-	//for (unsigned int i = 0; i < 64; ++i)
-	//{
-	//	outSigma1[i] = sigma1(inSigma1[i]);
-	//}
-
-	//// INPUT : sigma2 function
-	//inSigma2[0] = 0;
-	//inSigma2[1] = valMax;
-	//for (unsigned int i = 2; i < 64; ++i)
-	//{
-	//	inSigma2[i] = ((RAND_MAX * rand() + rand()) % (valMax - 1)) + 1; //on génère entre 1 et 2^32-2 inclus
-	//}
-	//for (unsigned int i = 0; i < 64; ++i)
-	//{
-	//	outSigma2[i] = sigma2(inSigma2[i]);
-	//}
-
-	//// INPUT : sigma3 function
-	//inSigma3[0] = 0;
-	//inSigma3[1] = valMax;
-	//for (unsigned int i = 2; i < 64; ++i)
-	//{
-	//	inSigma3[i] = ((RAND_MAX * rand() + rand()) % (valMax - 1)) + 1; //on génère entre 1 et 2^32-2 inclus
-	//}
-	//for (unsigned int i = 0; i < 64; ++i)
-	//{
-	//	outSigma3[i] = sigma3(inSigma3[i]);
-	//}
-
-	//// === Writting into the files ===
-	//// ch.txt file
-	//remove("ch.txt");
-	//fopen_s(&file, "ch.txt", "w");
-
-	//for (unsigned int j = 0; j < 3; j++)
-	//{
-	//	fprintf(file, "constant INPUTS_%d : vector32_t(0 to 63) := (\n", j + 1);
-	//	for (unsigned int i = 0; i < 63; ++i)
-	//	{
-	//		fprintf(file, "\tx\"%08x\",\n", inCh[j][i]);
-	//	}
-	//	fprintf(file, "\tx\"%08x\"\n\t);\n\n", inCh[j][63]);
-
-	//}
-
-	//fprintf(file, "constant OUTPUTS : vector32_t(0 to 63) := (\n");
-	//for (unsigned int i = 0; i < 63; ++i)
-	//{
-	//	fprintf(file, "\tx\"%08x\",\n", outCh[i]);
-	//}
-	//fprintf(file, "\tx\"%08x\"\n\t);\n\n", outCh[63]);
-
-	//fclose(file);
-
-	//// maj.txt file
-	//remove("maj.txt");
-	//fopen_s(&file, "maj.txt", "w");
-
-	//for (unsigned int j = 0; j < 3; j++)
-	//{
-	//	fprintf(file, "constant INPUTS_%d : vector32_t(0 to 63) := (\n", j + 1);
-	//	for (unsigned int i = 0; i < 63; ++i)
-	//	{
-	//		fprintf(file, "\tx\"%08x\",\n", inMaj[j][i]);
-	//	}
-	//	fprintf(file, "\tx\"%08x\"\n\t);\n\n", inMaj[j][63]);
-
-	//}
-
-	//fprintf(file, "constant OUTPUTS : vector32_t(0 to 63) := (\n");
-	//for (unsigned int i = 0; i < 63; ++i)
-	//{
-	//	fprintf(file, "\tx\"%08x\",\n", outMaj[i]);
-	//}
-	//fprintf(file, "\tx\"%08x\"\n\t);\n\n", outMaj[63]);
-
-	//fclose(file);
-
-	//// sigma0.txt file
-	//remove("sigma0.txt");
-	//fopen_s(&file, "sigma0.txt", "w");
-
-	//fprintf(file, "constant INPUTS : vector32_t(0 to 63) := (\n");
-	//for (unsigned int i = 0; i < 63; ++i)
-	//{
-	//	fprintf(file, "\tx\"%08x\",\n", inSigma0[i]);
-	//}
-	//fprintf(file, "\tx\"%08x\"\n\t);\n\n", inSigma0[63]);
-
-	//fprintf(file, "constant OUTPUTS : vector32_t(0 to 63) := (\n");
-	//for (unsigned int i = 0; i < 63; ++i)
-	//{
-	//	fprintf(file, "\tx\"%08x\",\n", outSigma0[i]);
-	//}
-	//fprintf(file, "\tx\"%08x\"\n\t);\n\n", outSigma0[63]);
-
-	//fclose(file);
-
-	//// sigma1.txt file
-	//remove("sigma1.txt");
-	//fopen_s(&file, "sigma1.txt", "w");
-
-	//fprintf(file, "constant INPUTS : vector32_t(0 to 63) := (\n");
-	//for (unsigned int i = 0; i < 63; ++i)
-	//{
-	//	fprintf(file, "\tx\"%08x\",\n", inSigma1[i]);
-	//}
-	//fprintf(file, "\tx\"%08x\"\n\t);\n\n", inSigma1[63]);
-
-	//fprintf(file, "constant OUTPUTS : vector32_t(0 to 63) := (\n");
-	//for (unsigned int i = 0; i < 63; ++i)
-	//{
-	//	fprintf(file, "\tx\"%08x\",\n", outSigma1[i]);
-	//}
-	//fprintf(file, "\tx\"%08x\"\n\t);\n\n", outSigma1[63]);
-
-	//fclose(file);
-
-	//// sigma2.txt file
-	//remove("sigma2.txt");
-	//fopen_s(&file, "sigma2.txt", "w");
-
-	//fprintf(file, "constant INPUTS : vector32_t(0 to 63) := (\n");
-	//for (unsigned int i = 0; i < 63; ++i)
-	//{
-	//	fprintf(file, "\tx\"%08x\",\n", inSigma2[i]);
-	//}
-	//fprintf(file, "\tx\"%08x\"\n\t);\n\n", inSigma2[63]);
-
-	//fprintf(file, "constant OUTPUTS : vector32_t(0 to 63) := (\n");
-	//for (unsigned int i = 0; i < 63; ++i)
-	//{
-	//	fprintf(file, "\tx\"%08x\",\n", outSigma2[i]);
-	//}
-	//fprintf(file, "\tx\"%08x\"\n\t);\n\n", outSigma2[63]);
-
-	//fclose(file);
-
-	//// sigma3.txt file
-	//remove("sigma3.txt");
-	//fopen_s(&file, "sigma3.txt", "w");
-
-	//fprintf(file, "constant INPUTS : vector32_t(0 to 63) := (\n");
-	//for (unsigned int i = 0; i < 63; ++i)
-	//{
-	//	fprintf(file, "\tx\"%08x\",\n", inSigma3[i]);
-	//}
-	//fprintf(file, "\tx\"%08x\"\n\t);\n\n", inSigma3[63]);
-
-
-
-	//fprintf(file, "constant OUTPUTS : vector32_t(0 to 63) := (\n");
-	//for (unsigned int i = 0; i < 63; ++i)
-	//{
-	//	fprintf(file, "\tx\"%08x\",\n", outSigma3[i]);
-	//}
-	//fprintf(file, "\tx\"%08x\"\n\t);\n\n", outSigma3[63]);
-
-	//fclose(file);
-
-	
+	// Écriture des fichiers de sortie
+	ecrireFichier("ch.txt", liste3Entrees, sortiesCh);
+	ecrireFichier("maj.txt", liste3Entrees, sortiesMaj);
+	ecrireFichier("sigma0.txt", liste1Entree, sortiesSigma0);
+	ecrireFichier("sigma1.txt", liste1Entree, sortiesSigma0);
+	ecrireFichier("sigma2.txt", liste1Entree, sortiesSigma0);
+	ecrireFichier("sigma3.txt", liste1Entree, sortiesSigma0);
 }
-
-
-// Functions for which we generate test vector
-u32 ch(u32 x, u32 y, u32 z)
-{
-	return (x&y) ^ ((~x)&z);
-}
-
-u32 maj(u32 x, u32 y, u32 z)
-{
-	return (x&y) ^ (x&z) ^ (y&z);
-}
-
-u32 rotr(u32 n, u32 x)
-{
-	return (x << (32 - n)) | (x >> n);
-}
-
-u32 shr(u32 n, u32 x)
-{
-	return x >> n;
-}
-
-u32 sigma0(u32 x)
-{
-	return rotr(2, x) ^ rotr(13, x) ^ rotr(22, x);
-}
-
-u32 sigma1(u32 x)
-{
-	return rotr(6, x) ^ rotr(11, x) ^ rotr(25, x);
-}
-
-u32 sigma2(u32 x)
-{
-	return rotr(7, x) ^ rotr(18, x) ^ shr(3, x);
-}
-
-u32 sigma3(u32 x)
-{
-	return rotr(17, x) ^ rotr(19, x) ^ shr(10, x);
-}
-
 
 void initialisationGenerateurNombresAleatoires()
 {
-	srand(time(NULL));
+	srand(unsigned(time(NULL)));
 }
 
-std::vector<std::vector<u32>> genererValeursEntree32bitsAleatoires(unsigned nEdgeCases, unsigned nEntrees, unsigned nValeurs)
+u32 genererValeurAleatoire()
 {
-	std::vector<std::vector<u32>> vecteurDEntrees;
-	for (int i = 0; i < nEntrees; i++)
+	return ((RAND_MAX * rand() + rand()) % (MAXIMUM_32BITS));
+}
+
+std::vector<u32> genererVecteurAleatoire(unsigned nValeurs)
+{
+	std::vector<u32> valeurs;
+	for (unsigned i = 0; i < nValeurs; i++)
 	{
-		std::vector<u32> valeurs;
-		for (int j = nEdgeCases; j < nValeurs; j++)
+		valeurs.push_back(genererValeurAleatoire());
+	}
+	return valeurs;
+}
+
+ListeVecteurs genererListeValeursAleatoires(unsigned nEntrees, unsigned nValeursCible)
+{
+	ListeVecteurs listeEntrees;
+	for (unsigned i = 0; i < nEntrees; i++)
+	{
+		listeEntrees.push_back(genererVecteurAleatoire(nValeursCible));
+	}
+	return listeEntrees;
+}
+
+ListeVecteurs genererListeCasLimitesDeBase(unsigned nEntrees)
+{
+	ListeVecteurs vecteurDEntrees(nEntrees);
+	unsigned nCasLimites = unsigned(pow(2.0, nEntrees));
+
+	for (unsigned i = 0; i < nCasLimites; i++)
+	{
+		for (unsigned j = 0; j < nEntrees; j++)
 		{
-			valeurs.push_back(((RAND_MAX * rand() + rand()) % (valMax + 1)));
+			if (bool(i & (0x00000001 << j)))
+				vecteurDEntrees[j].push_back(0xFFFFFFFF);
+			else
+				vecteurDEntrees[j].push_back(0x00000000);
 		}
-		valeurs.push_back(((RAND_MAX * rand() + rand()) % (valMax + 1)));
 	}
 	return vecteurDEntrees;
+}
+
+ListeVecteurs genererListeEntrees32bits(unsigned nEntrees, unsigned nValeurs)
+{
+	unsigned valeursRestantes = nValeurs;
+
+	// Cas limites
+	ListeVecteurs listeCasLimites = genererListeCasLimitesDeBase(nEntrees);
+	valeursRestantes -= unsigned(listeCasLimites[0].size());
+
+	// Valeurs aléatoires
+	ListeVecteurs listeValeursAleatoires = genererListeValeursAleatoires(nEntrees, valeursRestantes);
+
+	// Mise en commun
+	ListeVecteurs vecteurDEntrees(nEntrees);
+	for (unsigned i = 0; i < nEntrees; i++)
+	{
+		vecteurDEntrees[i].insert(vecteurDEntrees[i].end(), listeValeursAleatoires[i].begin(), listeValeursAleatoires[i].end());
+		vecteurDEntrees[i].insert(vecteurDEntrees[i].end(), listeCasLimites[i].begin(), listeCasLimites[i].end());
+	}
+	return vecteurDEntrees;
+}
+
+ListeVecteurs genererListeSorties(std::vector<u32>(*fonction)(std::vector<u32>), ListeVecteurs listeEntrees)
+{
+	unsigned nEntrees = unsigned(listeEntrees.size());
+	unsigned nValeurs = unsigned(listeEntrees[0].size());
+	ListeVecteurs listeSorties(1);
+
+	for (unsigned i = 0; i < nValeurs; i++)
+	{
+		std::vector<u32> vecteurEntree(nEntrees);
+		for (unsigned j = 0; j < nEntrees; j++)
+			vecteurEntree[j] = listeEntrees[j][i];
+		
+		std::vector<u32> vecteurSortie = fonction(vecteurEntree);
+		
+		for (unsigned j = 0; j < vecteurSortie.size(); j++)
+		{
+			if (listeSorties.size() < vecteurSortie.size())
+				listeSorties.resize(vecteurSortie.size());
+			listeSorties[j].push_back(vecteurSortie[j]);
+		}
+	}
+	return listeSorties;
+}
+
+void ecrireFichier(const char* nomFichier, ListeVecteurs entrees, ListeVecteurs sorties)
+{
+	// Initialisation du fichier
+	FILE* file;
+	remove(nomFichier);
+	fopen_s(&file, nomFichier, "w");
+	if (file == nullptr)
+		exit(1);
+
+	// Affichage de l'entrée
+	for (unsigned int i = 0; i < entrees.size(); i++)
+	{
+		fprintf(file, "constant INPUTS%s : vector32_t(0 to 63) := (\n", 
+			(entrees.size() > 0) ? ("_" + std::to_string(i)).c_str() : "");
+		for (unsigned int j = 0; j < NOMBRE_VALEURS - 1; j++)
+		{
+			fprintf(file, "\tx\"%08x\",\n", entrees[i][j]);
+		}
+		fprintf(file, "\tx\"%08x\"\n\t);\n\n", entrees[i][NOMBRE_VALEURS - 1]);
+	}
+
+	// Affichage de la sortie
+	for (unsigned int i = 0; i < sorties.size(); i++)
+	{
+		fprintf(file, "constant OUTPUTS%s : vector32_t(0 to 63) := (\n",
+			(sorties.size() > 0) ? ("_" + std::to_string(i)).c_str() : "");
+		for (unsigned int j = 0; j < 63; j++)
+		{
+			fprintf(file, "\tx\"%08x\",\n", sorties[i][j]);
+		}
+		fprintf(file, "\tx\"%08x\"\n\t);\n\n", sorties[i][NOMBRE_VALEURS - 1]);
+	}
+
+	// Désalocation de l'espace mémoire
+	fclose(file);
 }
